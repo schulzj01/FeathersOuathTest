046c55c46af19b0d1e0a6050e053e0397b45e414
schulzj01 <jeremy.schulz@noaa.gov>
2024-05-15 08:04:26 -0600

    Update authentication.js

--- src/authentication.js
+++ src/authentication.js
@@ -3,15 +3,10 @@ import { AuthenticationService, JWTStrategy } from '@feathersjs/authentication'
 
 import { oauth, OAuthStrategy } from '@feathersjs/authentication-oauth'
 
+//This is the repsponse handler.  You'll want to put in any custom code here to handle the SSO response
 class NOAAOnlineStrategy extends OAuthStrategy {
   async getEntityData(OAuthProfile, any, Params) {
     const baseData = await super.getEntityData(profile, existing, params)
-		console.log(baseData)
-		console.log(baseData)
-		console.log(baseData)
-		console.log(baseData)
-		throw new Error(baseData)
-		console.log(profile)
     return {
       ...baseData,
       // The GitHub profile image
@@ -24,13 +19,8 @@ class NOAAOnlineStrategy extends OAuthStrategy {
 
 export const authentication = (app) => {
   const authentication = new AuthenticationService(app)
-
-	console.warn(new NOAAOnlineStrategy());
   authentication.register('jwt', new JWTStrategy())
-  authentication.register('google', new OAuthStrategy())
-  authentication.register('github', new OAuthStrategy())
   authentication.register('noaaonline', new NOAAOnlineStrategy())
-
 
   app.use('authentication', authentication)
   app.configure(oauth())
